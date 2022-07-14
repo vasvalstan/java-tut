fdb254d37ffb22c393c47a396c676c883b6518cc
vasvalstan <stanciu327@gmail.com>
2022-07-14 22:45:29 +0300

    test calculator

---
+++ src/main/java/chapter3/TestResults.java
@@ -0,0 +1,30 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class TestResults {
+    public static void main(String[] args) {
+        //get values we know
+        System.out.println("Enter your test score");
+        Scanner scanner = new Scanner(System.in);
+        double score = scanner.nextDouble();
+
+        //if-else-if
+        char grade;
+
+        if(score < 60){
+            grade = 'F';
+        } else if (score < 70) {
+           grade = 'D';
+        } else if(score < 80) {
+            grade = 'C';
+        } else if (score < 90) {
+           grade = 'B';
+        } else {
+            grade = 'A';
+        }
+
+        System.out.println("Your grade is: " + grade);
+
+    }
+}
