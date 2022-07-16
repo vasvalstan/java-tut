60cdc07cf2bbfbc791b404f53278d430d9af6060
vasvalstan <stanciu327@gmail.com>
2022-07-16 07:41:18 +0300

    student average test score

---
+++ src/main/java/chapter4/AverageTestScores.java
@@ -0,0 +1,27 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class AverageTestScores {
+    public static void main(String[] args) {
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //process all students
+        for (int i=0; i<numberOfStudents; i++){
+
+            double total = 0;
+            for (int j=0; j<numberOfTests; j++){
+                System.out.println("Enter the score for Test #" + (j+1));
+                double score = scanner.nextDouble();
+                total = total + score;
+            }
+
+            double average = total/numberOfTests;
+            System.out.println("The test average for student #" +(i+1) + " is " + average);
+        }
+        scanner.close();
+    }
+}
