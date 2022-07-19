97d815b0325e1d5f21ade6bc4b7cd55f2e8130db
vasvalstan <stanciu327@gmail.com>
2022-07-19 23:50:18 +0300

    arrays

---
+++ src/main/java/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int myVariable = 5;
+        int [] myArray;
+        myArray = new int[3];
+        myArray[0] = 1;
+        myArray[1] = 2;
+        myArray[2] = 3;
+
+        System.out.println(myArray[0]);
+    }
+}
