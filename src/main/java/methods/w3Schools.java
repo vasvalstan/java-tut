6844dbef31f14e36d319718ec5ccc567ddda84d8
vasvalstan <stanciu327@gmail.com>
2022-07-29 13:04:48 +0300

    adding more fun

--- src/main/java/methods/w3Schools.java
+++ src/main/java/methods/w3Schools.java
@@ -1,11 +1,13 @@
 package methods;
 
 public class w3Schools {
-    static void myMethod(){
-        System.out.println("I got executed!");
+    static void myMethod(String fname, int age){
+        System.out.println(fname + " Stanciu " + " is age: " + age);
     }
 
     public static void main(String[] args) {
-        myMethod();
+        myMethod("Valentin", 29);
+        myMethod("Monica", 56);
+        myMethod("Ioan", 64);
     }
 }
---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void testDemo(){
+        System.out.println("Demo Test");
+    }
+}
